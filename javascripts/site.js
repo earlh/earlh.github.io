0db48f65f111f10d67ed194eae4b9b983e49d8e5
Earl Hathaway <github@earlh.com>
2026-09-07 13:23:51 -0700

    build: 2026 0907

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,24 @@
 // This is where it all goes :)
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.formspree-contact-form').forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      fetch(form.action, {
+        method: 'POST',
+        body: new FormData(form),
+        headers: { Accept: 'application/json' }
+      }).then(function (response) {
+        if (response.ok) {
+          form.closest('.contact-form-wrapper').innerHTML =
+            '<p class="thank-you">Thanks for reaching out &mdash; I\'ll get back to you soon.</p>';
+        } else {
+          alert('Oops, there was a problem submitting your form.');
+        }
+      }).catch(function () {
+        alert('Oops, there was a problem submitting your form.');
+      });
+    });
+  });
+});
