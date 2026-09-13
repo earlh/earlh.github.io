a9402e5551a08495abf50b8c61801df5a78e2731
Earl Hathaway <github@earlh.com>
2026-09-13 15:30:30 -0700

    build: 2026 0913

--- javascripts/site.js
+++ javascripts/site.js
@@ -13,6 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
         if (response.ok) {
           form.closest('.contact-form-wrapper').innerHTML =
             '<p class="thank-you">Thanks for reaching out &mdash; I\'ll get back to you soon.</p>';
+
+          window.dataLayer = window.dataLayer || [];
+          window.dataLayer.push({ event: 'contact_form_submitted' });
         } else {
           alert('Oops, there was a problem submitting your form.');
         }
